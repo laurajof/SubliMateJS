9fbb6ce8140e7d1bb96c03c3d3b15a7d2fb29d60
Laura Jofre <lajo1981@hotmail.com>
2022-09-06 19:58:40 -0300

    arrays

--- app.js
+++ app.js
@@ -1,55 +1,43 @@
-let suma = ""
-let stickerx1 = ""
-let stickerx10 = ""
-let stickerx20 = ""
-let resultado = ""
+let sticker = ""
+let precio = ""
+let cantidad = ""
+let articuloIngresado = ""
 
 //Bienvenida
 function solicitarNombre (){
-    let nombre = prompt("Ingrese su nombre");
-    alert("Bienvenido a SubliMate, " + nombre);
-  } 
-  
-  solicitarNombre();
-  
-  //comprar stickers
-  function comprarsticker(){
-      let sticker = Number (prompt("Qué desea comprar? \n 1: Sticker por unidad \n 2: Pack de Stickers x10 \n 3: Pack de Stickers x20"));
-  
-      if (sticker === 1) {
-        stickerx1 ++
-        valor = 70
-        alert("Excelente opción! Elegiste Sticker por unidad por un valor de $70");
-      } else if (sticker === 2) {
-        stickerx10 ++
-        valor = 500
-        alert("Excelente opción! Elegiste el pack de Sticker por 10 unidades por un valor de $500");
-      } else if (sticker === 3) {
-        stickerx20 ++
-        valor = 900
-        alert("Excelente opción! Elegiste el pack de Sticker por 20 unidades por un valor de $900");   
-      } else {
-        alert ("La opción ingresada no es correcta.");
-        alert ("Volvamos a empezar!");
-        comprarsticker()
-      }
+  let nombre = prompt("Ingrese su nombre");
+  alert("Bienvenido a la tienda de Stckers de SubliMate, " + nombre.toUpperCase() + "!" + "\nTenemos: \nStickers por unidad a $70 \nPack x 10 Stickers a $500 \nPack x 20 Stickers a $900.");
+}
+solicitarNombre();
+
+class Stickers {
+  constructor(sticker, precio, cantidad) {
+    this.sticker = sticker;
+    this.precio = parseFloat (precio);
+    this.cantidad = cantidad;
   }
+}
+const arrayStickers= [];
+let navegar = prompt("Que desea hacer? \n 1: Comprar Stickers \n 2: Finalizar compra \n 3: Salir ");
 
-comprarsticker()
+while (navegar === "1"){
+  sticker = prompt("ingrese el producto que desea comprar ");
+  precio = prompt("ingrese el precio del producto");
+  cantidad = prompt("ingrese cuantos quiere");
+  articuloIngresado = new Stickers (sticker, precio, cantidad);
+  arrayStickers.push(articuloIngresado);
+  console.log(arrayStickers);
+navegar = prompt("Elija una opcion: \n 1: Seguir comprando \n 2: Finalizar compra \n 3: Salir");
+} if (navegar === "3"){
+  alert ("Gracias por tu visita!")
 
-masopciones = Number (prompt("Como deseas continuar? \n 1: Seguir comprando \n 2: Finalizar compra \n 3: Salir"));
-
-while (masopciones === 1){
-    comprarsticker()
-    masopciones = Number (prompt("Como deseas continuar? \n 1: Seguir comprando \n 2: Finalizar compra \n 3: Salir"));
-} if (masopciones === 2){
-    let suma1 = stickerx1 * 70
-    let suma2 = stickerx10 * 500
-    let suma3 = stickerx20 * 900
-    resultado = (suma1+suma2+suma3)
-    alert ("Tu compra es por un total de $" + resultado + " ¡Pronto nos pondremos en contacto para realizar la entrega!");
-}else if (masopciones === 3){
-    alert ("Gracias por tu visita!");
 }
 
+let totalCompra = 0;
 
+for (i = 0; i < arrayStickers.length; i++) {
+  console.log(arrayStickers[i]);
+  totalCompra =
+    totalCompra + arrayStickers[i].precio * arrayStickers[i].cantidad;
+}
+alert("Gracias por su compra! El monto a abonar es de $  " + totalCompra);
