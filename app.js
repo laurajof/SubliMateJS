e27c0d60b45afea963de2aaabe364585c2b03151
Laura Jofre <lajo1981@hotmail.com>
2022-09-08 20:30:48 -0300

    arrays y filtro

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ let precio = ""
 let cantidad = ""
 let articuloIngresado = ""
 
+let resultado = ""
+let totalCompra = 0;
+
 //Bienvenida
 function solicitarNombre (){
   let nombre = prompt("Ingrese su nombre");
-  alert("Bienvenido a la tienda de Stckers de SubliMate, " + nombre.toUpperCase() + "!" + "\nTenemos: \nStickers por unidad a $70 \nPack x 10 Stickers a $500 \nPack x 20 Stickers a $900.");
+  alert("Bienvenido a la tienda de Stickers de SubliMate, " + nombre.toUpperCase() + "!" + "\nTenemos: \nStickers por unidad a $70 \nPack x 10 Stickers a $500 \nPack x 20 Stickers a $900.");
 }
-solicitarNombre();
 
 class Stickers {
   constructor(sticker, precio, cantidad) {
@@ -17,27 +19,69 @@ class Stickers {
     this.cantidad = cantidad;
   }
 }
+
+// ARRAYS
 const arrayStickers= [];
-let navegar = prompt("Que desea hacer? \n 1: Comprar Stickers \n 2: Finalizar compra \n 3: Salir ");
 
-while (navegar === "1"){
-  sticker = prompt("ingrese el producto que desea comprar ");
-  precio = prompt("ingrese el precio del producto");
-  cantidad = prompt("ingrese cuantos quiere");
-  articuloIngresado = new Stickers (sticker, precio, cantidad);
-  arrayStickers.push(articuloIngresado);
-  console.log(arrayStickers);
-navegar = prompt("Elija una opcion: \n 1: Seguir comprando \n 2: Finalizar compra \n 3: Salir");
-} if (navegar === "3"){
-  alert ("Gracias por tu visita!")
+const productos = [{
+    nombre: "Sticker x1",
+    precio: 70,
+    },
+    {nombre: "Sticker x10",
+    precio: 500,
+    },
+    {nombre: "Sticker x20",
+    precio: 900,
+    },
+];
 
+const carrito = [];
+
+solicitarNombre();
+
+let navegar = prompt("Que desea hacer? \n 1: Comprar Stickers \n 3: Mostrar producto destacado \n 4: Salir ");
+
+while (navegar !== "4"){
+    if (navegar == "1"){
+        sticker = prompt("ingrese el producto que desea comprar \n 1: Sticker x1 \n 2: Stickers x10 \n 3: Stickers x20");
+        if (sticker == "1") {
+            alert(`Excelente opción! Elegiste: ${productos[0].nombre} por un valor de $ ${productos[0].precio} `);
+            precioCompra = productos[0].precio
+        } else if (sticker == "2") {
+          alert(`Excelente opción! Elegiste: ${productos[1].nombre} por un valor de $ ${productos[1].precio} `);
+          precioCompra = productos[1].precio
+        } else if (sticker ==="3") {
+          alert(`Excelente opción! Elegiste: ${productos[2].nombre} por un valor de $ ${productos[2].precio} `);
+          precioCompra = productos[2].precio
+        } else {
+            alert ("ingrese una opción válida")
+            sticker = prompt("1: Sticker x1 \n 2: Stickers x10 \n 3: Stickers x20");
+        }
+        alert ("El precio del producto elejido es de $" + " " + precioCompra);
+
+        cantidad = prompt("ingrese cuantos quiere");
+
+        resultado = precioCompra * cantidad
+        totalCompra = resultado + totalCompra
+
+        articuloIngresado = new Stickers (sticker, precio, cantidad);
+      
+        arrayStickers.push(articuloIngresado);
+      
+        console.log(arrayStickers);
+    }
+    if (navegar == "2"){
+        alert ("Finalizó su compra con un total de $:" + " " + totalCompra + ". Nos contactaremos con usted a la brevedad.")
+    }
+    //Producto destacado
+    if (navegar === "3"){
+        productoDestacado = (productos.filter((e)=> e.precio < 100));
+        console.log(productoDestacado)
+        for (var i =0; i < productoDestacado.length; i++){
+            alert (`Nuestro producto destacado del mes es: ${productos[i].nombre} a un precio de $ ${productos[i].precio}`)
+        }
+    }
+    navegar = prompt("Elija una opcion: \n 1: Seguir comprando \n 2: Finalizar compra \n 3: Mostrar producto destacado \n 4: Salir");
 }
-
-let totalCompra = 0;
-
-for (i = 0; i < arrayStickers.length; i++) {
-  console.log(arrayStickers[i]);
-  totalCompra =
-    totalCompra + arrayStickers[i].precio * arrayStickers[i].cantidad;
-}
-alert("Gracias por su compra! El monto a abonar es de $  " + totalCompra);
+ 
+alert ("Gracias por tu visita!")
