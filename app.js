472c14309f24b17795264c03b480fcb819acbfa9
Laura Jofre <lajo1981@hotmail.com>
2022-09-22 21:44:24 -0300

    JSON y Storage

--- app.js
+++ app.js
@@ -1,37 +1,3 @@
-/* //constantes botones
-const button = document.querySelector("#x1");
-const para = document.createElement("p");
-const valor = document.createElement("p");
-para.innerHTML = "Sticker x unidad";
-valor.innerHTML = "Valor $70"; 
-
-const button10 = document.querySelector("#x10");
-const para10 = document.createElement("p");
-const valor10 = document.createElement("p");
-para10.innerHTML = "Stickers x10 unidades";
-valor10.innerHTML = "Valor $500";
-
-const button20 = document.querySelector("#x20");
-const para20 = document.createElement("p");
-const valor20 = document.createElement("p");
-para20.innerHTML = "Stickers x20 unidades";
-valor20.innerHTML = "Valor $900";
-
-//carrito
-button.onclick = function(){
-    document.getElementById("comprar").appendChild(para);
-    document.getElementById("valor").appendChild(valor);
-}
-
-button10.onclick = function(){
-    document.getElementById("comprar").appendChild(para10);
-    document.getElementById("valor").appendChild(valor10);
-}
-
-button20.onclick = function(){
-    document.getElementById("comprar").appendChild(para20);
-    document.getElementById("valor").appendChild(valor20);
-} */
 const datosStickers = [
   {
       id: 1,
@@ -99,17 +65,28 @@ function validarFormulario(){
   let celular = document.getElementById("celular").value;
   let cliente1 = new Cliente(nombre, email, celular);
   console.log(cliente1);
+  
+  const nombreCliente = JSON.stringify(nombre);
+  sessionStorage.setItem("datonombre", nombreCliente);
+  const emailCliente = JSON.stringify(email);
+  sessionStorage.setItem("datomail", emailCliente);
+  const celularCliente = JSON.stringify(celular);
+  sessionStorage.setItem("datocel", celularCliente);
+  console.log(nombreCliente)
   mostrar(cliente1);
 }
 
 function mostrar(cliente){
+  nombrebaja = sessionStorage.getItem(`datonombre`);
+  emailbaja = sessionStorage.getItem(`datomail`);
+  celbaja = sessionStorage.getItem(`datocel`);
+  console.log(nombrebaja)
   let formulario = document.getElementById("usuario");
   formulario.innerHTML = "";
-//Nuevo Cliente
+
   let nuevoCliente = document.createElement("div")
-  nuevoCliente.innerHTML = `<h1>Gracias ${cliente.nombre} por contactarnos! En breve recibiras un email en: ${cliente.mail}</h1>`;
-  
   nuevoCliente.className = "datos";
+  nuevoCliente.innerHTML = `<p>Gracias ${nombrebaja} por contactarnos! En breve recibiras un email en: ${emailbaja}</p>`;
   formulario.appendChild(nuevoCliente);
+  
 }
-const guardarcliente = JSON.stringify(nuevoCliente);
